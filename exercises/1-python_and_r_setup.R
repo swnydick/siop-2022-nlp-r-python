868eb65b19fc559c5cb07dcd75f91de1587d4c3d
Steven Nydick <steven.nydick@kornferry.com>
2022-02-04 15:02:17 -0600

    Update installation/usage instructions

--- exercises/1-python_and_r_setup.R
+++ exercises/1-python_and_r_setup.R
@@ -16,6 +16,9 @@ analyses_dir <- file.path(project_dir, "exercises")
 
 setwd(project_dir)
 
+# use renv to load the required packages
+renv::restore()
+
 # 2. Required Packages =========================================================
 
 # if you don't have the latest version of each package, please install them
@@ -26,6 +29,9 @@ packageVersion("keras")      # install.packages("keras")
 # we will always use the "pkg::fun()" syntax for reticulate for clarity.
 median(c(1, 2, 3, 4))        # if a package is loaded, you can use its name
 stats::median(c(1, 2, 3, 4)) # use the "::" syntax to not fully load the package
+
+# remove RETICULATE_PYTHON environment variable to make things easier
+Sys.unsetenv("RETICULATE_PYTHON")
 
 # 3. Setup Reticulate ==========================================================
 
@@ -53,10 +59,14 @@ stats::median(c(1, 2, 3, 4)) # use the "::" syntax to not fully load the package
 #        NORMALLY and not through homebrew)
 #     f. NEW installation of miniconda in R (if enabled AND no other version of
 #        Python has yet been flagged) with name: r-reticulate and after installing
-#        the numpy package
+#        the numpy package (if using renv, will install in renv and have
+#        RETICULATE_MINICONDA_PYTHON_ENVPATH set to renv version, otherwise will
+#        use base one)
 #     g. NEW installation of miniconda in R (if enabled AND no other version of
 #        Python has yet been flagged) with "required" module name and after
-#        installing the numpy package
+#        installing the numpy package (if using renv, will install in renv and have
+#        RETICULATE_MINICONDA_PYTHON_ENVPATH set to renv version, otherwise will
+#        use base one)
 #     h. The default version of python3 on the system
 #     i. Other common locations for python/conda on the system (this is where it
 #        will find the homebrew installation)
@@ -65,16 +75,18 @@ stats::median(c(1, 2, 3, 4)) # use the "::" syntax to not fully load the package
 #     a. Is NEW installation of miniconda IN r-reticulate environment OR
 #     b. Python version >= 2.7, architecture OK, AND has numpy > 1.6
 #     c. If everything else fails, choose python with architecture OK
-reticulate::py_discover_config()
+
+reticulate::py_discover_config() # why is this one chosen ?
 
 # You can fix the version of reticulate
 # - Tools -> Global Options  -> Python Interpreter -> Select
 # - Tools -> Project Options -> Python Interpreter -> Select
 Sys.getenv("RETICULATE_PYTHON")
+reticulate::py_discover_config() # why is this one chosen ?
 
 # also change Renviron to .Renviron in project and restart
 Sys.getenv("RETICULATE_PYTHON")
-reticulate::py_discover_config()
+reticulate::py_discover_config() # why is this one chosen ?
 
 # what if we have set the .Renviron AND fixed python in Project Options?
 
@@ -99,10 +111,12 @@ if(!file.exists(reticulate::miniconda_path())){
 siop_env <- "r-siop-nlp"
 
 # creating required environments (in R version of miniconda)
-reticulate::conda_create(envname = siop_env)
+reticulate::conda_create(envname = siop_env)  # reticulate::virtualenv_create()
+                                              # if using venv
 
 # activating FIRST environment (works)
-reticulate::use_condaenv(siop_env)
+reticulate::use_condaenv(condaenv = siop_env, # reticulate::use_virtualenv()
+                         required = TRUE)     # if using venv
 reticulate::py_discover_config()
 
 # activating OTHER python (works)
@@ -119,18 +133,45 @@ reticulate::py_discover_config()
 
 # restart R and initialize environment
 rstudioapi::restartSession()
-reticulate::use_condaenv(siop_env)
+
+# need to update renv to make this work (frustratingly doesn't always work)
+siop_env <- "r-siop-nlp"
+reticulate::use_condaenv(condaenv = siop_env,
+                         required = TRUE)
 reticulate::py_discover_config()
 
-# 4. Using Python in R Code ====================================================
+# if you are using renv, you can do the following, but be warned that it's very
+# difficult to NOT use the renv python once you've done this!
+# renv::use_python(python = file.path(reticulate::miniconda_path(), "envs", siop_env, "bin/python3"),
+#                  type   = "conda")
 
-# you can install modules (doesn't default into current environment?)
-reticulate::conda_install(packages = "numpy")      # bad
-reticulate::conda_install(envname  = siop_env,
-                          packages = "numpy")      # good
-reticulate:::condaenv_resolve()                    # default env to install
+# note: if using renv, it will force you to use renv/python/r-reticulate if
+#       you DON'T select python using renv::use_python OR reticulate::use_python
+# note: if using renv::use_python(), will put renv-python into RETICULATE_PYTHON
+#       environment variable, and you cannot change environment. Need to use
+#       renv::use_python and NOT reticulate::use_condaenv!
+# see:  https://rstudio.github.io/renv/articles/python.html
+# reticulate::conda_create("blah")
+# reticulate::use_condaenv("blah", required = TRUE) # works ... NO warning
+# reticulate::py_discover_config()                  # still OLD version
 
-# reticulate has pretty strong opinions about where you should work! :/
+# 4.Using Python and R Code ====================================================
+
+## a. Hello World --------------------------------------------------------------
+
+# how can we write a simple "hello world" function using reticulate?
+
+# way 1: use py_run_string (with a raw r string)
+reticulate::py_run_string(r"(print("Hello World"))")
+
+# way 2: import builtins module and use python methods/functions
+builtins <- reticulate::import_builtins(convert = FALSE)
+builtins$print("Hello World")
+
+# way 3: use repl_python and use python interpreter
+reticulate::repl_python() # print("Hello World")
+
+## b. Accessing Objects --------------------------------------------------------
 
 # you can use python in R pretty easily
 reticulate::repl_python() # >>> y = 2
@@ -143,26 +184,55 @@ x <- 14
 reticulate::py_run_string("print(r.x)")
 
 # note that py_run_string returns all of the python objects
-reticulate::py_run_string("y = 2 + 2")$r$x
+reticulate::py_run_string("y = 2 + 2")$y # same as reticulate::py
 
 # note: in python, use "object.method" to access methods/elements of objects
 #       in R, use python_object$method to access corresponding methods
 
+## c. Using Modules ------------------------------------------------------------
+
+# you can install modules (doesn't default into current environment?)
+reticulate::conda_install(packages = "numpy")      # bad (see "Package Plan")
+reticulate::conda_install(envname  = siop_env,
+                          packages = "numpy")      # good
+reticulate:::condaenv_resolve()                    # default env to install
+
+# reticulate has pretty strong opinions about where you should work! :/
+
 # you can import packages and run things from them
 np <- reticulate::import(module  = "numpy",
-                         convert = FALSE) # import package
-X  <- np$array(c(2, 4, 1, 3))             # run python code
-X$mean()                                  # apply method(s)
-y  <- reticulate::py_to_r(X)
+                         convert = FALSE)          # import package
+X  <- np$array(c(2, 4, 1, 3))                      # run python code
+X$mean()                                           # apply method(s)
+Y  <- reticulate::py_to_r(X)
+Y                                                  # is this an R object? restart R!
+
+# restart R and initialize environment
+rstudioapi::restartSession()                       # might need to FORCE a restart!
+siop_env <- "r-siop-nlp"
+reticulate::use_condaenv(siop_env)
+
+# try again!!
+builtins <- reticulate::import_builtins(convert = FALSE)
+np       <- reticulate::import(module  = "numpy",
+                               convert = FALSE)
+X        <- np$array(c(2, 4, 1, 3))             
+X$mean()                                  
+Y        <- reticulate::py_to_r(X)
+Y                                                  # NOW it works!
 
 # note: if you use convert = TRUE, the output that you get in the py_to_r function
 #       will be python output and not R output (it will do NOTHING)
-# note: you cannot load a module multiple times in different ways! (rule of reticulate)
+# note: you cannot load a module multiple times in different ways!
+# note: sometimes reticulate doesn't listen, then it's best to restart R (manually)!
 
 # your variable is a pointer to a python object, so will update if python does
-X$sort() # sort in place
-X        # is updated (in python)
-y        # is not updated (in R)
+Z        <- X # original object
+Z             # original x object
+X$sort()      # sort in place
+X             # is updated (in python)
+Y             # is not updated (in R)
+Z             # is updated (in python)
 
 # you can chain operations
 X$cumsum()$mean()
@@ -170,10 +240,21 @@ X$cumsum()$mean()
 # how do we add two objects together in python
 X2    <- X$copy()
 Y2    <- X$copy()
-Z2    <- reticulate::py_run_string(code    = "Z2 = r.X2 + r.Y2",
-                                   convert = FALSE)$Z2
+X2 + Y2                                                 # doesn't work
+np$add(X2, Y2)                                          # works
+reticulate::py_run_string(code    = "Z2 = r.X2 + r.Y2", # works
+                          convert = FALSE)$Z2
 
-# 5. Using R in Python Code ====================================================
+# so you can either
+# - run python code directly using py_run_string (with r objects accessible in
+#   the r python object)
+# - use module/class methods in R using the "$", putting the R objects that point
+#   to python objects in the function, and then converting the whole thing back
+#   to R when needed
+
+# but ... you generally want to use convert = FALSE and manually convert later!
+
+# 6. Using R in Python Code ====================================================
 
 # See 1-python_and_r_setup.py File
 
