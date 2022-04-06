fcc9a49eee416edef855700f969c09d99aac58d7
Tristan Wisner <wisnert@iMac.local>
2022-04-06 12:09:01 -0700

    updates to exercises

--- exercises/2-keras_and_nn.R
+++ exercises/2-keras_and_nn.R
@@ -8,7 +8,8 @@
 # 1. Setup / Required Packages =================================================
 require(keras)
 require(ggplot2)
-
+require(mlbench)
+require(caret)
 
 # 2. What is Keras? =================================================
 
@@ -23,7 +24,7 @@ require(ggplot2)
 
 # define a simple tensor in Keras
 set.seed(3125)
-a <- k_constant(matrix(sample(1:10, 6), 2, 3)); a
+a <- k_constant(matrix(sample(1:10, 6), 2, 3, byrow = TRUE)); a # this will take a few seconds as Keras starts
 set.seed(3125)
 a <- k_constant(sample(1:10, 6), shape = c(2,3)); a #equivalent
 
@@ -85,21 +86,47 @@ k_sigmoid(c)
 
 ### Implementing a basic feed-forward neural network for regression in R with Keras
 
-housing <- dataset_boston_housing()
+data(BostonHousing)
+dim(BostonHousing)
+head(BostonHousing)
+BostonHousing$chas <- as.numeric(as.character(BostonHousing$chas))
 
 # Split into train and test data
-x_train <- housing$train$x
-x_test  <- housing$test$x
-y_train <- housing$train$y
-y_test  <- housing$test$y
+bh_partition <- createDataPartition(BostonHousing$medv, p = 0.8, list = FALSE)
 
-n_x = dim(x_train)[2] # whatever dimension input data is
+bh_train <- BostonHousing[ bh_partition, ]
+bh_test <- BostonHousing[ -bh_partition, ]
+
+# First let's try a simple, OLS linear regression:
+
+lm_model <- train(medv ~ ., data = bh_train, method = "lm")
+y_pred_lm <- predict(lm_model, bh_test[ , 1:13])
+
+cor(y_pred_lm, bh_test[ , 14])
+lm_model # note RMSE, MAE
+
+ggplot(data.frame(y_pred_lm, y_test = bh_test[ , 14]),
+       aes(y = y_pred_lm, x = y_test)) + 
+  geom_point() +
+  theme_minimal()
+
+
+
+# Now solve regression problem with neural net:
+
+# convert train and test sets into matrices for keras
+bh_x_train <- as.matrix(bh_train[ , 1:13])
+bh_x_test <- as.matrix(bh_test[ , 1:13])
+bh_y_train <- as.matrix(bh_train[ , 14])
+bh_y_test <- as.matrix(bh_test[ , 14])
+
+n_x = dim(bh_x_train)[2] # whatever dimension input data is
 
 # Input layer
-basic_input <- layer_input(shape = n_x)
+nn_reg_input <- layer_input(shape = n_x)
 
 # Hidden layers
-basic_hidden <- basic_input |>
+nn_reg_hidden <- nn_reg_input |>
   layer_dense(units      = 50, 
               activation = "relu", 
               name       = "hidden0") |>
@@ -111,48 +138,128 @@ basic_hidden <- basic_input |>
               name       = "hidden2") 
 
 # Output layer
-basic_out   <- layer_dense(object     = basic_hidden,
+nn_reg_out   <- layer_dense(object     = nn_reg_hidden,
                          units      = 1, 
                          activation = "linear",
                          name       = "output")
 
 # put together (input layer and output layer)
-basic_model <- keras_model(basic_input, basic_out)
+nn_reg_model <- keras_model(nn_reg_input, nn_reg_out)
 
-summary(basic_model)
+summary(nn_reg_model)
 
 # Compile model
-compile(basic_model,
+compile(nn_reg_model,
         optimizer = optimizer_adam(),
         loss      = "mean_squared_error",
-        metrics   = c("mean_absolute_error", "cosine_proximity")
+        metrics   = c("mean_absolute_error", "mean_squared_error")
 )
 
 # Train model
 
-basic_history <- fit(object          = basic_model, # the model
-                     x               = x_train, # the training input 
-                     y               = y_train,   # the training output
-                     batch_size      = 50,      # the batch size
-                     validation_data = list(x_test, y_test), # LIST of validation x, y
-                     epochs          = 10,       # transits of training data through algorithm
+nn_reg_history <- fit(object          = nn_reg_model, # the model
+                     x               = bh_x_train, # the training input 
+                     y               = bh_y_train,   # the training output
+                     batch_size      = 25,      # the batch size
+                     validation_data = list(bh_x_test, bh_y_test), # LIST of validation x, y
+                     epochs          = 50,       # transits of training data through algorithm
                      shuffle         = TRUE,      # shuffle data before each epoch
                      view_metrics    = TRUE,      # plot how it's doing
                      verbose         = 1)         # progress bar
 
-print(basic_history)
+print(nn_reg_history)
 
-y_pred <- predict(basic_model, x_test)
-cor(y_pred, y_test)
+y_pred_nn_reg <- predict(nn_reg_model, bh_x_test)
+cor(y_pred_nn_reg, bh_y_test)
 
-ggplot(data.frame(y_pred, y_test),
-       aes(y = y_pred, x = y_test)) + 
+ggplot(data.frame(y_pred_nn_reg, bh_y_test),
+       aes(y = y_pred_nn_reg, x = bh_y_test)) + 
   geom_point() +
   theme_minimal()
 
-# pull out wall-of-text and put in rmd
-# compare boston housing regression model to a basic LM
-# add a quick classification example - throw in some different layers, activations
+
+#################################################################################
+
+### Now lets try a classification model
+
+data(PimaIndiansDiabetes)
+dim(PimaIndiansDiabetes)
+head(PimaIndiansDiabetes)
+
+# Split into train and test data
+pi_partition <- createDataPartition(PimaIndiansDiabetes$diabetes, p = 0.8, list = FALSE)
+
+pi_train <- PimaIndiansDiabetes[ pi_partition, ]
+pi_test <- PimaIndiansDiabetes[ -pi_partition, ]
+
+# Start with simple logistic regression model:
+
+logit_model <- train(factor(diabetes) ~ ., data = pi_train, method = "glm", family = "binomial")
+y_pred_logm <- predict(logit_model, pi_test[ , 1:8])
+
+confusionMatrix(y_pred_logm, pi_test[,9]) # note Accuracy, Kappa
+
+
+# Now lets try a neural network for binary classfication:
+
+# convert train and test sets into matrices for keras
+pi_x_train <- as.matrix(pi_train[ , 1:8])
+pi_x_test <- as.matrix(pi_test[ , 1:8])
+pi_y_train <- to_categorical(as.integer(pi_train[ , 9]) - 1, dtype = "int32")
+pi_y_test <- to_categorical(as.integer(pi_test[ , 9]) - 1, dtype = "int32")
+
+n_x = dim(pi_x_train)[2] # whatever dimension input data is
+
+# Input layer
+nn_class_input <- layer_input(shape = n_x)
+
+# Hidden layers
+nn_class_hidden <- nn_class_input |>
+  layer_dense(units      = 32, 
+              activation = "tanh", 
+              name       = "hidden0") |>
+  layer_dense(units      = 8, 
+              activation = "relu", 
+              name       = "hidden1") 
+
+# Output layer
+nn_class_out   <- layer_dense(object     = nn_class_hidden,
+                           units      = 2, 
+                           activation = "sigmoid",
+                           name       = "output")
+
+# put together (input layer and output layer)
+nn_class_model <- keras_model(nn_class_input, nn_class_out)
+
+summary(nn_class_model)
+
+# Compile model
+compile(nn_class_model,
+        optimizer = optimizer_adam(),
+        loss      = "binary_crossentropy",
+        metrics   = c("accuracy", "binary_crossentropy")
+)
+
+# Train model
+
+nn_class_history <- fit(object          = nn_class_model, # the model
+                           x               = pi_x_train, # the training input 
+                           y               = pi_y_train,   # the training output
+                           batch_size      = 32,      # the batch size
+                           validation_data = list(pi_x_test, pi_y_test), # LIST of validation x, y
+                           epochs          = 50,       # transits of training data through algorithm
+                           shuffle         = TRUE,      # shuffle data before each epoch
+                           view_metrics    = TRUE,      # plot how it's doing
+                           verbose         = 1)         # progress bar
+
+print(nn_class_history)
+
+y_pred_nn_class <- predict(nn_class_model, pi_x_test) %>% k_argmax() %>% as.matrix %>% as.factor
+pi_y_test <- pi_y_test %>% k_argmax() %>% k_cast("int32") %>% as.matrix %>% as.factor
+
+confusionMatrix(y_pred_nn_class, pi_y_test)
+#compare to logit:
+confusionMatrix(y_pred_logm, pi_test[,9]) # note Accuracy, Kappa
 
 
 
